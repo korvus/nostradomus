62f8a38bb2ef6a37bffe48386314fc791163bd6d
simon <ertel.simon@gmail.com>
2015-10-26 11:38:11 +0100

    fix

--- data/functions/seeAllElts.js
+++ data/functions/seeAllElts.js
@@ -1,45 +1,49 @@
-
 var myElements = undefined;
 
-function seeAllElts() {
-    if (myElements) {
+function hideAllElts() {
+    for (var i = 0; i < myElements.length; i++) {
+        myElements[i].style.outline = "none";
+    }
+}
 
-        for (var i = 0; i < myElements.length; i++) {
-            myElements[i].style.outline = "none";
+function displayAllElts() {
+    myElements = document.querySelectorAll("*");
+
+    //Based on https://html.spec.whatwg.org/multipage/indices.html#element-content-categories
+    let childrenPhrasing = ['abbr', 'b', 'bdi', 'bdo', 'button', 'cite', 'code', 'data', 'dfn', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'kbd', 'label', 'legend', 'mark', 'meter', 'output', 'p', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'small', 'span', 'strong', 'sub', 'summary', 'sup', 'time', 'u', 'var'];
+    let tableElts = ['table', 'tr', 'td', 'th', 'thead', 'tbody', 'tfoot', 'caption', 'colgroup'];
+
+    var color = "0f65da";
+
+    for (var i = 0; i < myElements.length; i++) {
+
+        if (childrenPhrasing.indexOf(myElements[i].nodeName.toLowerCase()) != -1) {
+            color = "ceda0f";
+        } else if (tableElts.indexOf(myElements[i].nodeName.toLowerCase()) > -1) {
+            color = "E03926";
+        } else {
+            color = "0f65da";
         }
 
+        myElements[i].style.outline = "1px solid #" + color;
+    }
+}
+
+function toggleElts() {
+
+    if (myElements) {
+
+        hideAllElts(myElements);
 
     } else {
 
-        myElements = document.querySelectorAll("*");
-
-        //Based on https://html.spec.whatwg.org/multipage/indices.html#element-content-categories
-        var childrenPhrasing = ['abbr', 'b', 'bdi', 'bdo', 'button', 'cite', 'code', 'data', 'dfn', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'kbd', 'label', 'legend', 'mark', 'meter', 'output', 'p', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'small', 'span', 'strong', 'sub', 'summary', 'sup', 'time', 'u', 'var'];
-        var tableElts = ['table', 'tr', 'td', 'th', 'thead', 'tbody', 'tfoot', 'caption', 'colgroup'];
-
-        var color = "0f65da";
-        for (var i = 0; i < myElements.length; i++) {
-
-            if (childrenPhrasing.indexOf(myElements[i].nodeName.toLowerCase()) != -1) {
-                color = "ceda0f";
-            } else if (tableElts.indexOf(myElements[i].nodeName.toLowerCase()) > -1) {
-                color = "E03926";
-            } else {
-                color = "0f65da";
-            }
-
-            myElements[i].style.outline = "1px solid #" + color;
-        }
+        displayAllElts();
 
     }
 }
 
 //By contextMenu
-self.on("click",function(){
-    seeAllElts();
-});
+self.on("click", toggleElts);
 
 //From the shortcut
-self.port.on('shortcut', function () {
-    seeAllElts();
-});
+self.port.on('shortcut', toggleElts);
