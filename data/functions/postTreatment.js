17c71f024fe653212132fd9d3f0fc1493a35cc3a
simon <ertel.simon@gmail.com>
2015-11-04 18:44:05 +0100

    parsing SVG implemented

--- data/functions/postTreatment.js
+++ data/functions/postTreatment.js
@@ -74,8 +74,31 @@ exports.forHiddenElts = function (feedBack) {
 
 
 exports.countCSS = function (feedBack) {
+
+    /*
+    <svg width="12cm" height="5.25cm" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
+
+<circle cx="300" cy="200" r="150" fill="red" stroke="black" stroke-width="1" />
+
+<path id= "a1" d="M300,200 v-150 a150,150 0 0,0 -150,150 z" fill="yellow" stroke="black" stroke-width="1" transform="rotate(180 300 200)"/>
+<path id= "a2" d="M300,200 v-150 a150,150 0 0,0 -150,150 z" fill="blue" stroke="black" stroke-width="1" transform="rotate(220 300 200)"/>
+<path id= "a3" d="M300,200 v-150 a150,150 0 0,0 -150,150 z" fill="green" stroke="black" stroke-width="1" transform="rotate(237 300 200)"/>
+<path id= "a4" d="M300,200 v-150 a150,150 0 0,0 -150,150 z" fill="maroon" stroke="black" stroke-width="1" transform="rotate(320 300 200)"/>
+<path id= "a5" d="M300,200 v-150 a150,150 0 0,0 -150,150 z" fill="red" transform="rotate(400 300 200)"/>
+
+    </svg>
+    */
+
+    /* > https://jsfiddle.net/cuzdv1ts/7/ */
+
     const structure = [
-          ["output", {}, feedBack.toString()]
+          ["output", {}, feedBack.toString()],
+          ["svg", { "width": "100px", "height": "100px", "viewBox": "0 0 300 300"},
+              [
+                  ["circle", { "cx": "150", "cy": "150", "r": "140", "fill": "#0f65da" }],
+                  ["path", { "id": "a1", "d": "M160,160 v-150 a140,140 0 0,0 -150,150 z", "fill": "#5f65ff", "stroke": "black" }]
+              ]
+          ]
     ];
 
     getPanel.andDisplay(structure);
--- data/panel/script.js
+++ data/panel/script.js
@@ -1,27 +1,39 @@
 //Array of Content
 const ac = self.options.toDisplay;
 const wrap = document.getElementById("content");
+const svg = /svg|circle|path/i;
+
 
 //ade : Array Dom Elements
 function deployDom(ade, container) {
     let adel = ade.length;
     let domn = [];
     for (var n = 0; n < adel; n++) {
-        domn[n] = document.createElement(ade[n][0]);
-        //If there is some attributes to set.
-        if (ade[n][1]) {
-            Object.getOwnPropertyNames(ade[n][1]).forEach(function (val, idx, array) {
-                domn[n].setAttribute(val, ade[n][1][val]);
+
+        //Create the root node
+        let tagName = ade[n][0];
+        if (svg.test(tagName)) {
+            domn[n] = document.createElementNS("http://www.w3.org/2000/svg", tagName);
+        } else {
+            domn[n] = document.createElement(tagName);
+        }
+        
+        //set attributes.
+        let attributes = ade[n][1];
+        if (attributes) {
+            Object.getOwnPropertyNames(attributes).forEach(function (val, idx, array) {
+                domn[n].setAttribute(val, attributes[val]);
             });
         }
 
+        //Include contents ty
         if (ade[n][2]) {
             //If it is an array, it mean it's an another included domNode
             if (ade[n][2] instanceof Array) {
                 deployDom(ade[n][2], domn[n]);
             }else{
-                let sTxt = document.createTextNode(ade[n][2]);
-                domn[n].appendChild(sTxt);
+                let txt = document.createTextNode(ade[n][2]);
+                domn[n].appendChild(txt);
             }
         }
         container.appendChild(domn[n]);
