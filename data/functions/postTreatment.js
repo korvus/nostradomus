53b900ce20a075763fc68fdd39fbe8064273c58f
simon <ertel.simon+github@gmail.com>
2015-11-10 01:33:54 +0100

    display infos about CSS
    still have to fix @ rules.

--- data/functions/postTreatment.js
+++ data/functions/postTreatment.js
@@ -101,6 +101,7 @@ exports.countCSS = function (feedBack) {
     let CSSlocal = feedBack[1][2];
 
     /* example of data:
+    Selectors / rules / url
     Array [[178,173,"http://www.foiredautomne.fr/extension/comexposiumdesign/design/comexposium/stylesheets/ScreenInner.css"],[15,15,"http://postitwar.me/r/css/home.css"]]
     Array [[3,2],[1,1]]
     Array [[80,66,"http://annuaireblogbd.com/ressources/css/atelier.css"],[187,154,"http://annuaireblogbd.com/ressources/css/calendar.css"]]
@@ -126,15 +127,19 @@ exports.countCSS = function (feedBack) {
         var labels = ["External file","Style Inline", "Same domain name"];
 
         feedBack[1].map(function(differentsKindofCSS){
-            
-            differentsKindofCSS.map(
+
+            styleByBlock = [];
+
+            differentsKindofCSS.filter(moreThanOne=>moreThanOne.length>0).map(
+
                 function(CSSanalysis){
+                    // drop previous styleByBlock array
                     dataPie.push(CSSanalysis[1]);
                     color.push(colorSet[iter]);
                     styleByBlock.push(["li",{},[
-                        ["a",{"href":"http://simonertel.net"},"simonertel.net"],
-                        ["span",{},"150 rules"],
-                        ["span",{},"152 selectors"]
+                        ["a",{"href":CSSanalysis[2],"target":"_blank"},CSSanalysis[2]],
+                        ["span",{}," "+CSSanalysis[1]+" rule(s) / "],
+                        ["span",{},CSSanalysis[0]+" selectors"]
                     ]]);
                 }
             );
@@ -150,7 +155,6 @@ exports.countCSS = function (feedBack) {
             iter++;
         });
 /*
-    <ul >
         <li>
             <h2>
                 <span>External file</span>
@@ -182,7 +186,6 @@ exports.countCSS = function (feedBack) {
                 <span>1240 rules / 1256 selectors</span>
             </h2>
         </li>
-    </ul>
 */
         SVG = cookPie( dataPie, widthSVG, heightSVG, cx, cy, rayon, color);
         toDisplay = [
