fb73657aca976e322a2c27799b882bf650e033ad
simon <ertel.simon+github@gmail.com>
2015-11-14 22:07:22 +0100

    selectors

--- data/functions/browserSide/crawlClassName.js
+++ data/functions/browserSide/crawlClassName.js
@@ -5,22 +5,49 @@
 
     var all = document.querySelectorAll("*");
     var i = 0;
-    var rootClassName = "";
+    var className = "";
+    var idName = "";
     var arrayOfClass = [];
-    var allClass = [];
+    var allSelector = [];
 
-    function add(rootClassName) {
-        if (rootClassName) {
-            arrayOfClass = rootClassName.split(" ");
-            arrayOfClass.map(a=>allClass.push(a));
+    var regex = /[A-Z]/;
+    var color = "0f65da";
+
+    function displayClass(nodeDom){
+        if(nodeDom.length > 0){
+            var display = confirm("Afficher les éléments concernés");
+            if(display){
+                for (var i = 0; i < nodeDom.length; i++) {
+                    nodeDom[i].style.outline = "1px solid #" + color;
+                }
+            }
         }
     }
 
-    while (all[i]) {
-        rootClassName = all[i].className;
-        add(rootClassName);
+    function add(rootClassName,idName) {
+        if (rootClassName) {
+            arrayOfClass = rootClassName.toString().split(" ");
+            arrayOfClass.filter(c=>regex.test(c)).map(a=>allSelector.push({"classValue":a}));
+        }
+        if(idName) 
+            if(regex.test(idName)) allSelector.push({"idValue":a});
+        }
+    }
+
+    function checkSelectors(){
+        allSelector.map(function(c){
+            var nodes = document.querySelectorAll("."+c.classValue);
+            displayClass(nodes);
+        });
+    }
+
+    while (all[i]){
+        className = all[i].className;
+        IdName = all[i].getAttribute("id");
+        add(className,idName);
         i++;
     }
 
-    allClass.map(c=>console.log(c));
+    checkSelectors();
+
 })();
