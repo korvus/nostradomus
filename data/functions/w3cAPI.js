6ba15254fcadcc4d8325891c8a81ff92094abb6d
simon <ertel.simon+github@gmail.com>
2015-11-20 01:33:09 +0100

    step ahead w3cAPI

--- data/functions/w3cAPI.js
+++ data/functions/w3cAPI.js
@@ -1,96 +1,60 @@
-const toPost = require("sdk/request").Request;
+const { Cc, Ci } = require('chrome');
+var xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
 
-function getAPI() {
 
-    //encodeURIComponent
-    //application/x-www-form-urlencoded not supported. Please use multipart/form-data.
-
-var fileParts = encodeURIComponent('<!DOCTYPE html><html><a id="a"><b id="b">hey!</b></a></html>');
-
-/*
-
-//https://gist.github.com/jquerygeek/3902461
-
-// easy way to get current pages HTML
-$.get('#', function(html) {
-
-    // emulate form post
-    var formData = new FormData();
-    formData.append('out', 'json');
-    formData.append('content', html);
-
-    // make ajax call
-    $.ajax({
-        url: "http://html5.validator.nu/",
-        data: formData,
-        dataType: "json",
-        type: "POST",
-        processData: false,
-        contentType: false,
-        success: function(data) {
-            console.log(data.messages); // data.messages is an array
-        },
-        error: function() {
-           console.warn(arguments);
-        }
-    });
-});
-
-**/
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://validator.nu/?out=json&doc=http%3A%2F%2Fhsivonen.com%2Ftest%2Fmoz%2Fmessages-types%2Finfo.svg", true);
-
-xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
-
-xhr.onload = function (e) {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      console.log(xhr.responseText);
-    } else {
-      console.error(xhr.statusText);
-    }
-  }
-};
-
-xhr.onerror = function (e) {
-  console.error(xhr.statusText);
-};
-
-xhr.send(null);
-
-/*
-
-Content-Type: multipart/form-data; boundary=---------------------------174151374030834 
-Content-Length: 346 -----------------------------174151374030834 
-Content-Disposition: form-data; name="showsource" yes -----------------------------174151374030834
-Content-Disposition: form-data; name="content" <!DOCTYPE html> <html> <head> <title>Test</title> </head> <body> <p></p> </body> </html> -----------------------------174151374030834
--- 
+/* 
+ * 
+ * If request could be functional
+ * var Request = require("sdk/request").Request;
+ *
+ *     var fileParts = encodeURIComponent('<!DOCTYPE html><html><a id="a"><b id="b">hey!</b></a></html>');
+ * 
+ *     var apiServer = toPost({
+ *         url: "https://validator.nu/?out=json",
+ *         //headers: "out=json",
+ *         overrideMimeType: "text/html; charset=utf-8",
+ *         contentType: "multipart/form-data; boundary=----nostradomus-----",
+ *         content: fileParts,
+ *         onComplete: function (response) {
+ *             console.log(response.json);
+ *             console.log(response.text);
+ *         }
+ *     });
+ * 
+ *     apiServer.post();
 */
 
 
-    var apiServer = toPost({
-        url: "https://validator.nu/",
-        overrideMimeType: "text/plain; charset=utf-8",
-        contentType: "multipart/form-data; boundary=]-nostradomusBoundary-[; ",
-        headers: {'Content-Disposition':'form-data; name="content" <!DOCTYPE html> <html> <head> <title>Test</title> </head> <body> <p></p> </body> </html>'},
-        headers: {'Content-Disposition':'form-data; name="out" json'},
-        onComplete: function (response) {
-            console.log(response.json);
-            console.log(response.text);
-            console.log(response.json);
+function getAPI(rawHTML) {
+
+    var fileParts = encodeURIComponent(rawHTML);
+
+    xhr.open("post", "https://validator.nu/?out=json", true);
+    xhr.setRequestHeader("Content-Type", "text/html;charset=UTF-8");
+
+    xhr.onload = function (e) {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log(xhr.responseText);
+        } else {
+          console.error(xhr.statusText);
         }
-    });
+      }
+    };
 
-    apiServer.post();
+    xhr.onerror = function (e) {
+      //Get the result
+      console.error(xhr.statusText);
+    };
 
+    xhr.send(fileParts);
 
 }
 
 // Treatment of the infos from view
 exports.andTreat = function (rawHTML) {
 
-    getAPI();
+    getAPI(rawHTML);
     //console.log(rawHTML);
 
 };
